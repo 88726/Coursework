3204f3ad4edf6082705fbc216d746767cdf22a77
88726 <88726@farnborough.ac.uk>
2019-08-14 20:48:17 +0100

    home v2 - students now contains CRUD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,40 +36,4 @@ public class Main {
             System.out.println("Disonnection error" + exception.getMessage());
         }
     }
-
-    public static void read() {
-        openDatabase("CourseworkDatabase.db");
-        try {
-            PreparedStatement ps = db.prepareStatement("SELECT userID, userName, userPassword FROM users");
-
-            ResultSet results = ps.executeQuery();
-            while (results.next()) {
-                int userID = results.getInt(1);
-                String userName = results.getString(2);
-                String userPassword = results.getString(3);
-                System.out.println(userID + " " + userName + " " + userPassword);
-
-            }
-        } catch (Exception exception) {
-            System.out.println("Database error" + exception.getMessage());
-        }
-        closeDatabase();
-    }
-
-    public static void insert() {
-        openDatabase("CourseworkDatabase.db");
-        try {
-            PreparedStatement ps = db.prepareStatement("INSERT into users (userName, userPassword) VALUES (?,?)");
-
-            ps.setString(1, "hannah123" );
-            ps.setString(2, "noThankYou");
-
-            ps.executeUpdate();
-        } catch (Exception exception) {
-            System.out.println("Database error" + exception.getMessage());
-        }
-        closeDatabase();
-    }
-    //test
-
 }
--- src/main/java/Students.java
+++ src/main/java/Students.java
@@ -1,3 +1,68 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Students {
 
+    public static void read() {
+
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT userID, userName, userPassword FROM users");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                int userID = results.getInt(1);
+                String userName = results.getString(2);
+                String userPassword = results.getString(3);
+                System.out.println(userID + " " + userName + " " + userPassword);
+
+            }
+        } catch (Exception exception) {
+            System.out.println("Database error" + exception.getMessage());
+        }
+
+    }
+
+    public static void insert() {
+
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT into users (userName, userPassword) VALUES (?,?)");
+
+            ps.setString(1, "hannah123" );
+            ps.setString(2, "noThankYou");
+
+            ps.executeUpdate();
+        } catch (Exception exception) {
+            System.out.println("Database error" + exception.getMessage());
+        }
+
+    }
+    public static void update(String userName, String userPassword, int userID) {
+
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE users SET userName = ?, userPassword = ? WHERE userID = ?");
+
+            ps.setString(1, userName );
+            ps.setString(2, userPassword);
+            ps.setInt(3, userID);
+
+            ps.executeUpdate();
+        } catch (Exception exception) {
+            System.out.println("Database error" + exception.getMessage());
+        }
+
+    }
+    public static void delete(int userID) {
+
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM users WHERE userID = ?");
+
+            ps.setInt(1, userID );
+
+            ps.executeUpdate();
+        } catch (Exception exception) {
+            System.out.println("Database error" + exception.getMessage());
+        }
+
+    }
 }
+
